24103cbed4e5dd548567f9be555b77a114d6de8b
Mujtaba <mujtaba.aldebes@hotmail.com>
2019-03-09 21:44:03 +0100

    Catalan Square

--- Kattis/Java/catalanSquare/CatalanSquare.java
+++ Kattis/Java/catalanSquare/CatalanSquare.java
@@ -1,31 +1,20 @@
 //https://open.kattis.com/problems/catalansquare
+// https://en.wikipedia.org/wiki/Catalan_number
 
 package catalanSquare;
 import java.math.BigInteger;
-
 import kattio.Kattio;
-import profile.codeProfiler;
 
 public class CatalanSquare {
-	static Kattio io = new Kattio(System.in, System.out);	
-	
-	public static void solution(){
+	static Kattio io = new Kattio(System.in, System.out);
+
+	public static void main(String[] args) {
 		int N = io.getInt();
 		BigInteger[] c = new BigInteger[(N == 0 ? 1 : N+1)];
 		c[0] = BigInteger.ONE;
-		for (int i = 1; i < N +1; i++) {
+		for (int i = 1; i < N +1; i++) { // The Catalan numbers satisfy the recurrence relations
 			c[i] = (c[i-1].multiply(BigInteger.valueOf(4*i+2)).divide(BigInteger.valueOf(i+2)));
 		}
 		System.out.println(c[c.length-1]);
-	}
-	
-	public static void main(String[] args) {
-		try {
-			codeProfiler.timeIt("code 0", () -> solution());	
-		} catch (Exception e) {
-			System.out.println("Error main");
-		}	
-
-		 
 	}
 }
