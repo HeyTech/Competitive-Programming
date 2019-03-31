6c53561122b6327ce82159d31ba589e819807e88
Mujtaba <mujtaba.aldebes@hotmail.com>
2019-04-01 01:19:37 +0200

    rename

--- Kattis/Python/different_problem/different_problem.py
+++
@@ -1,4 +0,0 @@
-import operator
-for _ in range(3):
-    print(abs(operator.sub(*map(int, input().split()))))
-
--- Kattis/Python/quality_adjusted_life_year/qaly.py
+++
@@ -1,6 +0,0 @@
-# https://open.kattis.com/problems/qaly
-s = 0
-for _ in range(int(input())):
-    q,y = map(float, input().split())
-    s += q * y
-print("%0.3f" % s)
