4fd1a9b034357482017f2edb2c54415323f8e1a3
Mujtaba Aldebes <mujtaba.aldebes@hotmail.com>
2020-12-07 12:24:30 +0100

    remove print outs

--- AdventOfCode/2020/days/day4.h
+++ AdventOfCode/2020/days/day4.h
@@ -32,9 +32,6 @@ public:
     std::string buf;
     std::size_t valid_passports{0};
     for (auto const &p : passport_) {
-      std::cout << "------------------------------------------------------"
-                << std::endl;
-      std::cout << p << std::endl;
       valid = true;
       std::stringstream ss{p};
       // iterate thought passport configurations, full loop..
@@ -45,19 +42,16 @@ public:
         if (key == "byr") {
           int year{std::stoi(value)};
           if (!aoc::checks::digit_between_values<int>(1920, 2002, year)) {
-            std::cout << "byr: invalid" << std::endl;
             valid = false;
           }
         } else if (key == "iyr") {
           int year{std::stoi(value)};
           if (!aoc::checks::digit_between_values<int>(2010, 2020, year)) {
-            std::cout << "iyr: invalid" << std::endl;
             valid = false;
           }
         } else if (key == "eyr") {
           int year{std::stoi(value)};
           if (!aoc::checks::digit_between_values<int>(2020, 2030, year)) {
-            std::cout << "eyr: invalid" << std::endl;
             valid = false;
           }
         } else if (key == "hgt") {
@@ -67,32 +61,25 @@ public:
           if (cm != std::string::npos) {
             int const hgt{std::stoi(value.substr(0, cm))};
             if (!aoc::checks::digit_between_values<int>(150, 193, hgt)) {
-              std::cout << "hgt(cm): invalid" << std::endl;
               valid = false;
             }
-            //            std::cout << value << " cm " << hgt << std::endl;
           } else if (in != std::string::npos) {
             int const hgt{std::stoi(value.substr(0, in))};
             if (!aoc::checks::digit_between_values<int>(59, 76, hgt)) {
-              std::cout << "hgt(in): invalid" << std::endl;
               valid = false;
             }
-            //            std::cout << value << " in " << hgt << std::endl;
           } else {
             valid = false;
           }
         } else if (key == "hcl") {
           if (value.at(0) != '#') {
-            std::cout << "hcl #: invalid" << std::endl;
             valid = false;
           } else if (value.length() != 7) {
-            std::cout << "hcl 7: invalid" << std::endl;
             valid = false;
           } // not done
           std::string const color{value.substr(1, value.size())};
           if (color.find_first_not_of("0123456789abcdef") !=
               std::string::npos) {
-            std::cout << "hcl a-f: invalid " << color << std::endl;
             valid = false;
           }
         } else if (key == "ecl") {
@@ -101,22 +88,17 @@ public:
                                      value == "grn" || value == "hzl" ||
                                      value == "oth"};
           if (!valid_eye_color) {
-            std::cout << "ecl: invalid" << std::endl;
             valid = false;
           }
         } else if (key == "pid") {
           if (value.size() != 9) {
-            std::cout << "pid 9: invalid" << std::endl;
             valid = false;
           } else if (!aoc::checks::is_digits(value.substr(1, value.size()))) {
-            std::cout << "pid digits: invalid" << std::endl;
             valid = false;
           }
         }
       }
       if (valid) {
-        std::cout << "********************** Valid ************************"
-                  << std::endl;
         valid_passports++;
       }
     }
