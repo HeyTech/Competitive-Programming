c8c43f21dd51d2264b8d677eafc2803fa88044df
Mujtaba Aldebes <mujtaba.aldebes@hotmail.com>
2020-12-12 18:30:29 +0100

    Day 12 task a

--- AdventOfCode/2020/main.cpp
+++ AdventOfCode/2020/main.cpp
@@ -1,35 +1,17 @@
-#include "aoc/aoc.h"
-#include "aoc/read_input.h"
 #include <string>
 #include <vector>
 
-#include "days/day1.h"
-#include "days/day2.h"
-#include "days/day3.h"
-#include "days/day4.h"
-#include "days/day5.h"
-#include "days/day6.h"
-#include "days/day7.h"
-
-#include "days/day11.h"
+#include "aoc/read_input.h"
+#include "days/day12.h"
 
 int main() {
+  //  std::vector<std::string> inputs{
+  //      "F10", "N3", "F7", "R90", "F11",
+  //  };
 
   std::vector<std::string> inputs;
   aoc::read_input::each_line(inputs);
-  //  std::vector<std::string> inputs{
-  //      "L.LL.LL.LL",
-  //      "LLLLLLL.LL",
-  //      "L.L.L..L..",
-  //      "LLLL.LL.LL",
-  //      "L.LL.LL.LL",
-  //      "L.LLLLL.LL",
-  //      "..L.L.....",
-  //      "LLLLLLLLLL",
-  //      "L.LLLLLL.L",
-  //      "L.LLLLL.LL",
-  //  };
-  Day11<std::string> day(inputs);
+  Day12<std::string> day(inputs);
   day.task1();
   day.task2();
 
